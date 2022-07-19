388ec4d5738759a6832819d1beab8635e21d384f
EatyMeatyYum <jason.bourne.ta@gmail.com>
2022-07-19 07:00:27 -0400

    Update basher.js

--- basher.js
+++ basher.js
@@ -1,10 +1,10 @@
 console.log("ABash loaded successfully");
 nexusclient.datahandler().send_command("who here");
 
-if(!get_variable("basharrrPrioList")) {
+if(!nexusclient.variables().get("basharrrPrioList))) {
   nexusclient.display_notice("There's no bashing prio list!!!", "red");
   var prioList = {};
-  set_variable("basharrrPrioList", prioList);
+  nexusclient.variables().set("basharrrPrioList", prioList);
 } else {
   nexusclient.display_notice("Bashing Prio List loaded into repo!", "green");
 }
