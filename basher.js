63f207d3ffc173b31dbbdd8e5c8bef4427432b76
EatyMeatyYum <jason.bourne.ta@gmail.com>
2022-07-19 12:46:35 -0400

    Update basher.js

--- basher.js
+++ basher.js
@@ -8,3 +8,35 @@ if(!nexusclient.variables().get("basharrrPrioList")) {
 } else {
   nexusclient.display_notice("Bashing Prio List loaded into repo!", "green");
 }
+
+// Trigger: When a denizen is slain
+
+var denSlain = function(denizen) {
+  var slainDenizen = denizen;
+  var currentArea = nexusclient.variables().get("currentArea");
+  var prioList = nexusclient.variables().get("basharrrPrioList");
+
+  if(prioList[currentArea]) {
+    // Area already exists in prio list
+    var denizenList = prioList[currentArea];
+    console.log(denizenList);
+    if(!denizenList.includes(slainDenizen)) {
+      nexusclient.display_notice("[ABASH]: New denizen added", "yellow");
+      denizenList.push(slainDenizen);
+      prioList[currentArea] = denizenList;
+      nexusclient.variables().set("basharrrPrioList", prioList);
+    }
+  } else {
+    // Area does not exist in prio list
+    var denizenList = [];
+    denizenList.push(slainDenizen);
+    prioList[currentArea] = denizenList;
+    console.log(prioList);
+    nexusclient.variables().set("basharrrPrioList", prioList);
+    nexusclient.display_notice("[ABASH]: New area added", "yellow");
+    nexusclient.display_notice("[ABASH]: New denizen added", "yellow");
+  }
+
+  nexusclient.datahandler().send_command("attackThings");
+}
+eventBus.subscribe('denizenSlain', denSlain, 'denSlain');
